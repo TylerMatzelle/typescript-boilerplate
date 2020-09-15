52f4c8f6d86af8ed89997c14fb422b4f46275f17
Tyler Matzelle <tmatzelle16560@alumni.focuscollege.com>
2020-09-14 19:30:25 -0700

    Configured scripts for fix and no-fix variants

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 function helloworld(message: string) {
-    console.log("Hello World");
+    console.log("Hello World!");
 
-    let updatedmessage = { f: 1, b: 2 };
+    const updatedmessage = { f: 1, b: 2 };
 
     return 0;
 }
